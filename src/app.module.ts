6935dc84f9b7aa5818dc9a49455a54bcd1ef7371
Kuljira Kaewpradub <kuljira6869@gmail.com>
2026-08-27 10:04:50 +0700

    Add ProductsModule and HealthModule to AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { OrdersModule } from './orders/orders.module';
 import { AuthModule } from './auth/auth.module';
+import { ProductsModule } from './products/products.module';
+import { HealthModule } from './health/health.module';
 
 @Module({
   imports: [OrdersModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
+  HealthModule,
+  ProductsModule,
+  
 })
 export class AppModule {}
