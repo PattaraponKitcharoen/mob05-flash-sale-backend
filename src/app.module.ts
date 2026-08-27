6b4320898e433c3da9f2e5d0eeaa54e343a148e3
Pattarapon Kitcharoen <pattarapon.kitcharoen@gmail.com>
2026-08-27 10:28:31 +0700

    chore: remove app controller and service (no use)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,13 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { OrdersModule } from './orders/orders.module';
 import { AuthModule } from './auth/auth.module';
 import { ProductsModule } from './products/products.module';
 import { HealthModule } from './health/health.module';
 
 @Module({
-  imports: [OrdersModule, 
-            AuthModule,
-            HealthModule,
-            ProductsModule,],
-  controllers: [AppController],
-  providers: [AppService],
-  
-  
+  imports: [OrdersModule,
+    AuthModule,
+    HealthModule,
+    ProductsModule,],
 })
-export class AppModule {}
+export class AppModule { }
