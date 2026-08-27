2711c2578e78c0f5010e0bf206a7ecd4a53acc10
Pattarapon Kitcharoen <pattarapon.kitcharoen@gmail.com>
2026-08-27 10:34:34 +0700

    chore: import RedisModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,15 @@ import { OrdersModule } from './orders/orders.module';
 import { AuthModule } from './auth/auth.module';
 import { ProductsModule } from './products/products.module';
 import { HealthModule } from './health/health.module';
+import { RedisModule } from './redis/redis.module';
 
 @Module({
-  imports: [OrdersModule,
+  imports: [
+    RedisModule,
+    OrdersModule,
     AuthModule,
     HealthModule,
-    ProductsModule,],
+    ProductsModule
+  ],
 })
 export class AppModule { }
